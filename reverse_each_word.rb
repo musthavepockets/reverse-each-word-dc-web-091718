f928b900300278ac835cc44a1d71852b725804d0
cd <coraldarling@outlook.com>
2018-09-04 18:07:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-  ((string.split).collect do |word| word.reverse)
+  (string.split).collect(&:join) do |word| word.reverse
   end
-  string.join" "
 end
+#groups.map!(&:join)
